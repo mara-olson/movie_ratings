e0225f4d51cc66f314227ac2aa7ddc6f62b0594e
mara-olson <mara.c.olson@gmail.com>
2022-04-21 20:40:04 -0700

    Adding homepage html and route

--- crud.py
+++ crud.py
@@ -22,6 +22,10 @@ def create_movie(title, overview, release_date, poster_path):
 
     return movie
 
+def return_all_movies():
+    return Movie.query.all()
+
+
 def create_rating(user, movie, score):
     """Create and return a new rating."""
 
